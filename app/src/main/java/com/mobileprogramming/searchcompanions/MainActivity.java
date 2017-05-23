eeaa93bcd3f58e892b77a03fd6de9d0a0f631a5d
Italobrunomelo <italobrunomelo@hotmail.com>
2017-05-23 20:00:53 -0300

    Fragment List 2

--- app/src/main/java/com/mobileprogramming/searchcompanions/MainActivity.java
+++ app/src/main/java/com/mobileprogramming/searchcompanions/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_companions_list);
         fragmentList = (CompanionsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_companions_list);
 
     }

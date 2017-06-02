6826b15f12ec077331c932e3363450ace9543cff
Italobrunomelo <italobrunomelo@hotmail.com>
2017-06-02 18:39:01 -0300

    material design

--- app/src/main/java/com/mobileprogramming/luxuryescort/fragments/FragmentEscortsList.java
+++ app/src/main/java/com/mobileprogramming/luxuryescort/fragments/FragmentEscortsList.java
@@ -6,18 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.mobileprogramming.luxuryescort.R;
+import com.mobileprogramming.luxuryescort.model.Escorts;
 
 /**
  * Created by italo on 24/05/2017.
  */
 
 public class FragmentEscortsList extends Fragment {
+
+    private ListView listEscort;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_escorts_list, container, false);
+
 
 
         return view;
--- app/src/main/java/com/mobileprogramming/luxuryescort/fragments/FragmentEscortsRegister.java
+++ app/src/main/java/com/mobileprogramming/luxuryescort/fragments/FragmentEscortsRegister.java
@@ -6,8 +6,16 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mobileprogramming.luxuryescort.R;
+import com.mobileprogramming.luxuryescort.dao.EscortsDAO;
+import com.mobileprogramming.luxuryescort.model.Escorts;
 
 /**
  * Created by italo on 17/05/2017.
@@ -15,14 +23,64 @@ import com.mobileprogramming.luxuryescort.R;
 
 public class FragmentEscortsRegister extends Fragment {
 
-
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_escorts_register, container, false);
 
+        final EditText mEditTextName = (EditText) view.findViewById(R.id.editTextName);
+        final EditText mEditTextInformation = (EditText) view.findViewById(R.id.editTextInformation);
+        final EditText mEditTextPhone = (EditText) view.findViewById(R.id.editTextPhone);
+        final EditText mEditTextAge = (EditText) view.findViewById(R.id.editTextAge);
+        final EditText mEditTextLocation = (EditText) view.findViewById(R.id.editTextLocation);
 
+        TextView mTextViewUploadPhoto = (TextView) view.findViewById(R.id.textViewUploadPhoto);
+
+        final Switch mSwitchStatus = (Switch) view.findViewById(R.id.switchStatus);
+
+        Button mButtonLocationEscort = (Button) view.findViewById(R.id.buttonLocationEscort);
+        Button mButtonSaveEscort = (Button) view.findViewById(R.id.buttonSaveEscort);
+
+        ImageView mImageViewPhoto = (ImageView) view.findViewById(R.id.imageViewPhoto);
+
+        mButtonSaveEscort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EscortsDAO mEscortDAO = new EscortsDAO(getActivity());
+                Escorts mEscort = new Escorts();
+
+                mEscort.setmName(mEditTextName.getText().toString());
+                mEscort.setmInformation(mEditTextInformation.getText().toString());
+                mEscort.setmContact(mEditTextPhone.getText().toString());
+                mEscort.setmAge(mEditTextAge.getText().toString());
+                mEscort.setmLocation(mEditTextLocation.getText().toString());
+
+                /*mEscortDAO.insert(mEscort);
+                OnRefreshFormOK activity = (OnRefreshFormOK) getActivity();
+                activity.refresh();
+
+                if (!isLandScape())
+                    getActivity().finish();
+                Toast.makeText(getActivity(),"Usuário inserido com sucesso!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            Escorts student = (Escorts) bundle.getSerializable("escort");
+            //carregar nome
+            etName.setText(student.getName());
+            etEmail.setText(student.getEmail());
+            etPhone.setText(student.getPhone());
+            //carregar email
+            //carregar telefone
+        } */
+
+                //return view;
+            }
+
+        });
         return view;
     }
 }
